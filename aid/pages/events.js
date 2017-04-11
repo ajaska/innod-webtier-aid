5b95b3d113687d61c8458980c6e3efa8ca85ab97
David Nguyen <d_nguyen@berkeley.edu>
2017-04-11 03:31:55 -0700

    refactor components to component/ directory

--- aid/pages/events.js
+++ aid/pages/events.js
@@ -1,55 +1,17 @@
 import React from 'react'
 import Helmet from 'react-helmet'
 import { config } from 'config'
+import EventsList from 'components/pages/EventsList'
 
-function Event(props) {
-  return (
-    <div>
-      {props.name}
-    </div>
-  )
-}
-
-export default class EventsList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {data: []};
-  }
-
-  componentDidMount() {
-    this.getEventData();
-  } 
-
-  getEventData() {
-    fetch('data.json')
-      .then(res => res.json())
-      .then(json => {
-        this.setState({
-          data: json
-        });
-      });
-  }
-
+export default class EventsPage extends React.Component {
   render() {
-    var events = this.state.data;
-    var eventsList = this.state.data.map(function(event) {
-      return (
-        <Event 
-          key={event.id}
-          name={event.name}
-          description={event.description}
-          location={event.location}
-          startTime={event.startTime}
-          endTime={event.endTime}
-        />
-      )          
-    });
-
     return (
-      <div className="EventsList">
-        {eventsList}
+      <div>
+        Hello
+        <EventsList />
       </div>
-    );
+    )
   }
 }
+
 
