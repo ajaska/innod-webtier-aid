d06f3eb5321b89e1d5b46e4b688a84c09920caa3
Bohui Moon <bohuimoon@gmail.com>
2017-04-11 13:15:09 -0700

    Remove example files and update gitignore

---
+++ aid/pages/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router'
+import { prefixLink } from 'gatsby-helpers'
+import { config } from 'config'
+
+class App extends React.Component
+{
+    render ()
+    {
+        return (<div>hello</div>);
+    }
+}
+
+export default App;
